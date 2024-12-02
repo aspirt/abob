fd6ebeb23614198b2b2f4067ee3b0a82964ed6ce
aspirt <nnnnaspirin@gmail.com>
2024-12-02 15:38:34 +0500

    ebenboben

---
+++ students/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Student
+
+
+class StudentForm(forms.ModelForm):
+  class Meta:
+    model = Student
+    fields = ['student_number', 'first_name', 'last_name', 'email', 'field_of_study', 'gpa']
+    labels = {
+      'student_number': 'Номер студента',
+      'first_name': 'Имя',
+      'last_name': 'Фамилия',
+      'email': 'Электронная почта',
+      'field_of_study': 'Направление обучения',
+      'gpa': 'Средний балл'
+    }
+    widgets = {
+      'student_number': forms.NumberInput(attrs={'class': 'form-control'}),
+      'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+      'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+      'email': forms.EmailInput(attrs={'class': 'form-control'}),
+      'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
+      'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
+    }
+
+
